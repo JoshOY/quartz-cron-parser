c973f41291644b80e9faaa558b09a96c6abd15d5
Josh Ouyang <me@joshoy.org>
2021-05-31 15:35:21 +0800

    export as module

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import nearley from 'nearley';
+import grammar from './grammar';
+
+export type QuartzCronValidationResult = {
+    result: QuartzCronField[];
+    error: null;
+} | {
+    result: null;
+    error: Error;
+};
+
+export type QuartzCronField = {
+  field: 'seconds' | 'minutes' | 'hours' | 'dayOfMonth' | 'month' | 'dayOfWeek' | 'years';
+  mode: 'every' | 'noSpecific' | 'specific' | 'increment' | 'range' | 'daysBeforeEndOfMonth' | 'lastweekDay' | 'nearestWeekdayOfMonth' | 'nthWeekDayOfMonth',
+  value: number | number[] | '*' | '?';
+};
+
+export function parse(cronExpression: string): QuartzCronValidationResult {
+  // Create a Parser object from our grammar.
+  const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar as any));
+  try {
+    // Parse the cron expression
+    parser.feed(`${cronExpression}`.trim().toUpperCase());
+    // Get result
+    const result = parser.results[0];
+    let dayFieldSpecifiedCount = 0;
+    dayFieldSpecifiedCount += ((result[3].mode !== 'noSpecific') ? 1 : 0);
+    dayFieldSpecifiedCount += ((result[5].mode !== 'noSpecific') ? 1 : 0);
+    if (dayFieldSpecifiedCount !== 1) {
+      throw new Error(`Support for specifying both a day-of-week and a day-of-month value is not complete (you'll need to use the '?' character in one of these fields).`);
+    }
+    return {
+      result,
+      error: null,
+    };
+  } catch (e) {
+    return {
+      result: null,
+      error: e,
+    };
+  }
+}
+
+export function validate(cronExpression: string): boolean {
+  const result = parse(cronExpression);
+  return result.error == null;
+}
