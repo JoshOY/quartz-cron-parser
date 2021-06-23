88f2e8257c04d95437ad4e248d845fc49ff85be7
Josh Ouyang <me@joshoy.org>
2021-06-23 15:59:57 +0800

    v0.1.2

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,32 @@
 // rollup.config.js
+import path from 'path';
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
 
-const commonPlugins = [typescript(), commonjs(), nodeResolve()];
+const commonPlugins = [typescript({
+  include: [
+    './src/**/*.ts',
+  ],
+  rollupCommonJSResolveHack: true,
+  tsconfig: path.resolve(__dirname, './tsconfig.json'),
+  useTsconfigDeclarationDir: true,
+}), commonjs(), nodeResolve()];
 
 export default {
-  input: 'src/index.ts',
+  input: path.resolve(__dirname, './src/index.ts'),
   output: [
     {
-      file: 'dist/index.js',
+      file: path.resolve(__dirname, './dist/index.js'),
       format: 'cjs',
     },
     {
-      file: 'dist/index.min.js',
+      file: path.resolve(__dirname, './dist/index.min.js'),
       format: 'cjs',
       plugins: [ terser() ],
+      sourcemap: true,
     },
   ],
   plugins: [...commonPlugins],
